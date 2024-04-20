646bb194803288c6f060c2a3ac1c4dc76c5e3bf7
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-04-20 00:17:57 -0300

    refactor: move instance creation to a helper method

--- src/adapters/email-validator-adapter.spec.ts
+++ src/adapters/email-validator-adapter.spec.ts
@@ -1,24 +1,28 @@
 import validator from 'validator'
 import { EmailValidatorAdapter } from './email-validator-adapter'
 
+const makeEmailValidatorAdapter = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter()
+}
+
 describe('EmailValidator Adapter', () => {
   test('Should return false if validator returns false', () => {
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
-    const emailValidatorAdapter = new EmailValidatorAdapter()
+    const emailValidatorAdapter = makeEmailValidatorAdapter()
     const isValid = emailValidatorAdapter.isValid('invalid_email@mail.com')
 
     expect(isValid).toBe(false)
   })
 
   test('Should return true if validator returns true', () => {
-    const emailValidatorAdapter = new EmailValidatorAdapter()
+    const emailValidatorAdapter = makeEmailValidatorAdapter()
     const isValid = emailValidatorAdapter.isValid('valid_email@mail.com')
 
     expect(isValid).toBe(true)
   })
 
   test('Should call validator with correct email', () => {
-    const emailValidatorAdapter = new EmailValidatorAdapter()
+    const emailValidatorAdapter = makeEmailValidatorAdapter()
     const isEmailSpy = jest.spyOn(validator, 'isEmail')
     emailValidatorAdapter.isValid('any_email@mail.com')
 
