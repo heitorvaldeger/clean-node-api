248d1bcaeaf5a666345b2bce6342ed9968e16777
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-04-20 00:10:07 -0300

    test: shoud return true if validator returns true

--- src/adapters/email-validator-adapter.spec.ts
+++ src/adapters/email-validator-adapter.spec.ts
@@ -1,10 +1,19 @@
+import validator from 'validator'
 import { EmailValidatorAdapter } from './email-validator-adapter'
 
 describe('EmailValidator Adapter', () => {
   test('Shoud return false if validator returns false', () => {
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const emailValidatorAdapter = new EmailValidatorAdapter()
     const isValid = emailValidatorAdapter.isValid('invalid_email@mail.com')
 
     expect(isValid).toBe(false)
+  })
+
+  test('Shoud return true if validator returns true', () => {
+    const emailValidatorAdapter = new EmailValidatorAdapter()
+    const isValid = emailValidatorAdapter.isValid('valid_email@mail.com')
+
+    expect(isValid).toBe(true)
   })
 })
