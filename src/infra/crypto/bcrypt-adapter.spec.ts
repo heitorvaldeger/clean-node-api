2055d446e25ee859b4f0657a599ab4897e0c6fc4
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-04-24 22:30:41 -0300

    test: ensure BcryptAdapter throws if bcrypt throws

--- src/infra/crypto/bcrypt-adapter.spec.ts
+++ src/infra/crypto/bcrypt-adapter.spec.ts
@@ -22,4 +22,14 @@ describe('Bcrypt Adapter', () => {
     const hash = await sut.encrypt('any_value')
     expect(hash).toBe('hashed_value')
   })
+
+  test('Should throws if bcrypt throws', async () => {
+    const sut = new BcryptAdapter()
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => {
+      throw new Error()
+    })
+
+    const promise = sut.encrypt('any_value')
+    await expect(promise).rejects.toThrow()
+  })
 })
