7898f65098a01f61b3bbc452bd8e8d02c4a0948f
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-04-24 22:37:51 -0300

    chore: add jest configs for mongodb

--- jest.config.mjs
+++ jest.config.mjs
@@ -102,7 +102,7 @@ const config = {
   // notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
-  // preset: undefined,
+  preset: '@shelf/jest-mongodb',
 
   // Run tests from one or more projects
   // projects: undefined,
