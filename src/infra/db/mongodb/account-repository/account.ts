8f30ec408d9d9e8f89aeafea1bb214fa4d82ea5c
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-04-25 23:17:16 -0300

    feat: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,26 @@
+import { WithId } from 'mongodb'
+import { IAddAccountRepository } from '../../../../data/interfaces/add-account-repository'
+import { IAddAccountModel } from '../../../../data/usecases/db-add-account-interfaces'
+import { IAccountModel } from '../../../../domain/model/account'
+import { MongoHelper } from '../helpers/mongodb-helper'
+
+export class AccountMongoRepository implements IAddAccountRepository {
+  async add (account: IAddAccountModel): Promise<IAccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const { insertedId } = await accountCollection.insertOne(account)
+    const accountData = await accountCollection.findOne<WithId<IAccountModel>>({
+      _id: insertedId
+    })
+
+    if (!accountData) {
+      throw new Error('Account not found')
+    }
+
+    const { _id, ...rest } = accountData
+
+    return ({
+      ...rest,
+      id: _id.toHexString()
+    })
+  }
+}
