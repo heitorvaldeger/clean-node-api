19cda1caa9e96981847b808c292880b9d8f05fd6
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-05-02 00:06:35 -0300

    test: ensure throw an error when account not found

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -28,4 +28,22 @@ describe('Account Mongo Repository', () => {
     expect(account.email).toBe('any_email@mail.com')
     expect(account.password).toBe('any_password')
   })
+
+  test('Should throw an error when account not found', async () => {
+    jest.spyOn(MongoHelper, 'getCollection').mockImplementationOnce((name) => {
+      const accountCollection = MongoHelper.getCollection(name)
+      accountCollection.findOne = jest.fn(async () => null)
+      return accountCollection
+    })
+
+    const sut = new AccountMongoRepository()
+
+    const accountAdd = sut.add({
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    })
+
+    await expect(accountAdd).rejects.toThrow('Account not found')
+  })
 })
