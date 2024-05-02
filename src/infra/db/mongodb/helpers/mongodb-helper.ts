90bf76e4cdbdb3065defd86007bb10250ee0b2b6
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-05-02 00:13:38 -0300

    feat: ensure throw an error if MongoDB uri is not defined when connecting'

--- src/infra/db/mongodb/helpers/mongodb-helper.ts
+++ src/infra/db/mongodb/helpers/mongodb-helper.ts
@@ -3,6 +3,9 @@ import { Collection, MongoClient } from 'mongodb'
 export const MongoHelper = {
   client: null as unknown as MongoClient,
   async connect (uri: string = ''): Promise<void> {
+    if (!uri) {
+      throw new Error('MongoDB uri is not defined')
+    }
     this.client = await MongoClient.connect(uri)
   },
   async disconnect (): Promise<void> {
