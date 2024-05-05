1b9867978743b813104356d1ed68ae0c0084ecee
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-05-05 18:48:04 -0300

    refactor: make server error return the error stack

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -37,7 +37,7 @@ export class SignUpController implements IController {
 
       return ok(account)
     } catch (error) {
-      return serverError()
+      return serverError(error as Error)
     }
   }
 }
