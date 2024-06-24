04b6894b2001fe1980d4f84d94d954f22e17666b
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-06-24 16:46:15 -0300

    feat: ensure AccountPostgresRepository update the account accessToken on updateAccessToken success

--- src/infra/db/postgres/account/account-postgres-repository.ts
+++ src/infra/db/postgres/account/account-postgres-repository.ts
@@ -1,8 +1,8 @@
 import { IAccountModel, IAddAccountModel, IAddAccountRepository } from '../../../../data/usecases/add-account/db-add-account-interfaces'
-import { ILoadAccountByEmailRepository } from '../../../../data/usecases/authentication/db-authentication-interfaces'
+import { ILoadAccountByEmailRepository, IUpdateAccessTokenRepository } from '../../../../data/usecases/authentication/db-authentication-interfaces'
 import { PostgresHelper } from '../helpers/postgres-helper'
 
-export class AccountPostgresRepository implements IAddAccountRepository, ILoadAccountByEmailRepository {
+export class AccountPostgresRepository implements IAddAccountRepository, ILoadAccountByEmailRepository, IUpdateAccessTokenRepository {
   async add (account: IAddAccountModel): Promise<IAccountModel> {
     const accountTable = PostgresHelper.getTable('accounts')
     const accountData = await accountTable.insert<IAccountModel>(account).returning('*')
@@ -18,5 +18,19 @@ export class AccountPostgresRepository implements IAddAccountRepository, ILoadAc
     const account = PostgresHelper.getTable('accounts').where('email', email).first<IAccountModel>()
 
     return await new Promise(resolve => { resolve(account) })
+  }
+
+  async updateAccessToken (id: string, accessToken: string): Promise<void> {
+    const updatedRows = await PostgresHelper.getTable('accounts')
+      .where('id', id)
+      .update({
+        accessToken
+      }, '*')
+
+    if (!(updatedRows.length > 0)) {
+      throw new Error('Account not found')
+    }
+
+    return updatedRows[0]
   }
 }
