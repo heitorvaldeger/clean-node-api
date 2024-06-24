c26311f64903778d81465332ca073306282ec2fe
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-06-24 20:02:58 -0300

    feat: add postgres connection in main

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,8 +1,10 @@
-import { MongoHelper } from '../infra/db/mongodb/helpers/mongodb-helper'
+import MongoHelper from '../infra/db/mongodb/helpers/mongodb-helper'
+import { PostgresHelper } from '../infra/db/postgres/helpers/postgres-helper'
 import env from './config/env'
 
 void MongoHelper.connect(env.mongoUrl)
   .then(async () => {
+    PostgresHelper.connect()
     const app = (await import('./app')).default
     app.listen(5050, () => { console.log(`Server running in port ${env.port}`) })
   })
