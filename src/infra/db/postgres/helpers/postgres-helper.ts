eba1ded4a1c249ec49ddfaf476bff366ebf66292
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-06-24 20:15:49 -0300

    feat: ensure PostgresHelper throw an error if Postgres don\'t connect throws

--- src/infra/db/postgres/helpers/postgres-helper.ts
+++ src/infra/db/postgres/helpers/postgres-helper.ts
@@ -4,19 +4,23 @@ import dotenv from 'dotenv'
 export const PostgresHelper = {
   client: null as unknown as Knex,
   connect () {
-    dotenv.config()
-    const postgresPort = Number(process.env.POSTGRES_PORT)
-    this.client = knex({
-      client: 'pg',
-      connection: {
-        host: process.env.POSTGRES_HOST,
-        database: process.env.POSTGRES_DATABASE,
-        user: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        port: postgresPort,
-        ssl: false
-      }
-    })
+    try {
+      dotenv.config()
+      const postgresPort = Number(process.env.POSTGRES_PORT)
+      this.client = knex({
+        client: 'pg',
+        connection: {
+          host: process.env.POSTGRES_HOST,
+          database: process.env.POSTGRES_DATABASE,
+          user: process.env.POSTGRES_USER,
+          password: process.env.POSTGRES_PASSWORD,
+          port: postgresPort,
+          ssl: false
+        }
+      })
+    } catch (error) {
+      throw new Error('Unable to connect to database')
+    }
   },
   async disconnect () {
     await this.client.destroy()
