b01be336fe0a9a7cf2c163c0bcdbe8b0a3b5c9ca
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-06-28 01:15:13 -0300

    feat: ensure LogPostgresRepository create an error log on success

---
+++ src/infra/db/postgres/log/log-postgres-repository.ts
@@ -0,0 +1,11 @@
+import { ILogErrorRepository } from '../../../../data/interfaces/db/log/log-error-repository'
+import { PostgresHelper } from '../helpers/postgres-helper'
+
+export class LogPostgresRepository implements ILogErrorRepository {
+  async logError (stack: string): Promise<void> {
+    await PostgresHelper.getTable('errors').insert({
+      stack,
+      date: new Date()
+    })
+  }
+}
--- src/main/factories/signup/signup-factory.ts
+++ src/main/factories/signup/signup-factory.ts
@@ -1,7 +1,7 @@
 import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../../infra/crypto/bcrypt-adapter/bcrypt-adapter'
-import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository'
 import { AccountPostgresRepository } from '../../../infra/db/postgres/account/account-postgres-repository'
+import { LogPostgresRepository } from '../../../infra/db/postgres/log/log-postgres-repository'
 import { LogControllerDecorator } from '../../../presentation/controllers/decorators/log-controller-decorator'
 import { IController } from '../../../presentation/controllers/interfaces/controller'
 import { SignUpController } from '../../../presentation/controllers/signup/signup-controller'
@@ -11,5 +11,5 @@ export const makeSignUpFactory = (): IController => {
   const bcrypter = new BcryptAdapter()
   const addAccountRepository = new AccountPostgresRepository()
   const addAccount = new DbAddAccount(bcrypter, addAccountRepository)
-  return new LogControllerDecorator(new SignUpController(addAccount, makeSignUpValidation()), new LogMongoRepository())
+  return new LogControllerDecorator(new SignUpController(addAccount, makeSignUpValidation()), new LogPostgresRepository())
 }
