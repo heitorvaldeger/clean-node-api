8a7f6c73b1d5c21afb64a1cf117ce72d22df1588
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-06-28 00:44:02 -0300

    refactor: change login repository

--- src/main/factories/login/login-factory.ts
+++ src/main/factories/login/login-factory.ts
@@ -2,17 +2,17 @@ import env from '../../config/env'
 import { DbAuthentication } from '../../../data/usecases/authentication/db-authentication'
 import { BcryptAdapter } from '../../../infra/crypto/bcrypt-adapter/bcrypt-adapter'
 import { JwtAdapter } from '../../../infra/crypto/jwt-adapter/jwt-adapter'
-import { AccountMongoRepository } from '../../../infra/db/mongodb/account/account-mongo-repository'
 import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repository'
 import { LogControllerDecorator } from '../../../presentation/controllers/decorators/log-controller-decorator'
 import { IController } from '../../../presentation/controllers/interfaces/controller'
 import { LoginController } from '../../../presentation/controllers/login/login-controller'
 import { makeLoginValidation } from './login-validation-factory'
+import { AccountPostgresRepository } from '../../../infra/db/postgres/account/account-postgres-repository'
 
 export const makeLoginFactory = (): IController => {
-  const accountMongoRepository = new AccountMongoRepository()
+  const accountPostgresRepository = new AccountPostgresRepository()
   const bcryptAdapter = new BcryptAdapter()
   const jwtAdapter = new JwtAdapter(env.jwtSecret)
-  const dbAuthentication = new DbAuthentication(accountMongoRepository, bcryptAdapter, jwtAdapter, accountMongoRepository)
+  const dbAuthentication = new DbAuthentication(accountPostgresRepository, bcryptAdapter, jwtAdapter, accountPostgresRepository)
   return new LogControllerDecorator(new LoginController(makeLoginValidation(), dbAuthentication), new LogMongoRepository())
 }
