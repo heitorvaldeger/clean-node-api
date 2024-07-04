74749ae05d2168d1754887dcc0097c96c5fdbeca
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-07-03 23:35:02 -0300

    test: add IsStringValidation to login route

--- src/main/factories/controllers/auth/login/login-validation-factory.spec.ts
+++ src/main/factories/controllers/auth/login/login-validation-factory.spec.ts
@@ -1,6 +1,7 @@
 import { EmailValidatorAdapter } from '../../../../../infra/email-validator/email-validator-adapter'
 import { EmailValidation, RequiredFieldValidation, ValidationComposite } from '../../../../../validations'
 import { IValidation } from '../../../../../validations/interfaces/validation'
+import { IsStringValidation } from '../../../../../validations/is-string/is-string-validation'
 import { makeLoginValidation } from './login-validation-factory'
 
 jest.mock('../../../../../validations/validation-composite/validation-composite')
@@ -12,6 +13,7 @@ describe('LoginValidator Factory', () => {
     const validations: IValidation[] = []
     for (const field of ['email', 'password']) {
       validations.push(new RequiredFieldValidation(field))
+      validations.push(new IsStringValidation(field))
     }
 
     validations.push(new EmailValidation('email', new EmailValidatorAdapter()))
