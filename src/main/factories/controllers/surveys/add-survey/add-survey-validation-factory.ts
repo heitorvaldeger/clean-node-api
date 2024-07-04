6bc267914afb62fa0959fee48b81ad7e8b9d6cb4
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-07-03 23:49:18 -0300

    feat: add IsStringValidation to survey route

--- src/main/factories/controllers/surveys/add-survey/add-survey-validation-factory.ts
+++ src/main/factories/controllers/surveys/add-survey/add-survey-validation-factory.ts
@@ -1,10 +1,13 @@
-import { RequiredFieldValidation, ValidationComposite } from '../../../../../validations'
+import { IsStringValidation, RequiredFieldValidation, ValidationComposite } from '../../../../../validations'
 import { IValidation } from '../../../../../validations/interfaces/validation'
 
 export const makeAddSurveyValidation = (): IValidation => {
   const validations: IValidation[] = []
   for (const field of ['question', 'answers']) {
     validations.push(new RequiredFieldValidation(field))
+    if (field === 'question') {
+      validations.push(new IsStringValidation(field))
+    }
   }
 
   return new ValidationComposite(validations)
