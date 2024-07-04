d49dc1408afed662859e3a5e2dc6d4f574dba7b9
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-07-03 23:29:05 -0300

    test: ensure IsStringValidation not return if validation succeeds

--- src/validations/is-string/is-string-validation.spec.ts
+++ src/validations/is-string/is-string-validation.spec.ts
@@ -4,8 +4,15 @@ import { IsStringValidation } from './is-string-validation'
 describe('IsString Validation', () => {
   test('Should return a InvalidParamError if validation fails', () => {
     const sut = new IsStringValidation('any_field')
-    const error = sut.validate(1)
+    const error = sut.validate(false)
 
     expect(error).toEqual(new InvalidParamError('any_field'))
+  })
+
+  test('Should not return if validation succeeds', () => {
+    const sut = new IsStringValidation('any_field')
+    const isValid = sut.validate('any_field')
+
+    expect(isValid).toBeFalsy()
   })
 })
