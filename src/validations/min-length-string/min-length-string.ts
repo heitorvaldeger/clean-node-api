dce3b279bd4945caf8f66f6f7728e8f8ab5840f0
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-07-04 00:50:41 -0300

    feat: add MinLengthStringValidation

---
+++ src/validations/min-length-string/min-length-string.ts
@@ -0,0 +1,17 @@
+import { IValidation } from '../interfaces/validation'
+import { InvalidParamError } from '../../presentation/errors'
+
+export class MinLengthStringValidation implements IValidation {
+  constructor (
+    private readonly fieldName: string,
+    private readonly stringLength: number
+  ) {}
+
+  validate (input: any): Error | null {
+    if (input[this.fieldName].length < this.stringLength) {
+      return new InvalidParamError(this.fieldName)
+    }
+
+    return null
+  }
+}
