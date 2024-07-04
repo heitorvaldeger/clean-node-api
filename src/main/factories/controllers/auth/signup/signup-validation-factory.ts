0c32d8f04ee318161b15a1a4084e56d73bfdc14f
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-07-03 23:38:07 -0300

    feat: add IsStringValidation to signup route

--- src/main/factories/controllers/auth/signup/signup-validation-factory.ts
+++ src/main/factories/controllers/auth/signup/signup-validation-factory.ts
@@ -1,11 +1,12 @@
 import { EmailValidatorAdapter } from '../../../../../infra/email-validator/email-validator-adapter'
-import { EmailValidation, RequiredFieldValidation, ValidationComposite, CompareFieldsValidation } from '../../../../../validations'
+import { EmailValidation, RequiredFieldValidation, ValidationComposite, CompareFieldsValidation, IsStringValidation } from '../../../../../validations'
 import { IValidation } from '../../../../../validations/interfaces/validation'
 
 export const makeSignUpValidation = (): IValidation => {
   const validations: IValidation[] = []
   for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
     validations.push(new RequiredFieldValidation(field))
+    validations.push(new IsStringValidation(field))
   }
 
   validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
--- src/validations/index.ts
+++ src/validations/index.ts
@@ -1,4 +1,5 @@
 export * from './compare-fields/compare-fields-validation'
 export * from './email-validation/email-validation'
 export * from './required-field/required-field-validation'
+export * from './is-string/is-string-validation'
 export * from './validation-composite/validation-composite'
