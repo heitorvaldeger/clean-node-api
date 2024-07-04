e4c53207e4cc48d5179dccb24c374ca16cb0ceed
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-07-04 00:51:00 -0300

    test: add MinLengthStringValidation

---
+++ src/validations/min-length-string/min-length-string-validator.spec.ts
@@ -0,0 +1,22 @@
+import { InvalidParamError } from '../../presentation/errors'
+import { MinLengthStringValidation } from './min-length-string'
+
+describe('MinLengthString Validation', () => {
+  test('Should return a InvalidParamError if validation fails', () => {
+    const sut = new MinLengthStringValidation('any_field', 5)
+    const error = sut.validate({
+      any_field: 'any'
+    })
+
+    expect(error).toEqual(new InvalidParamError('any_field'))
+  })
+
+  test('Should not return if validation succeeds', () => {
+    const sut = new MinLengthStringValidation('any_field', 5)
+    const isValid = sut.validate({
+      any_field: 'any_field'
+    })
+
+    expect(isValid).toBeFalsy()
+  })
+})
