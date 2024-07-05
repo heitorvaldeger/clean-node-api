a068367f56a0da362deb275df10978f8305eca53
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-07-04 23:32:34 -0300

    test: ensure MinLengthStringValidator return a InvalidParamError if field is undefined

--- src/validations/min-length-string/min-length-string-validator.spec.ts
+++ src/validations/min-length-string/min-length-string-validator.spec.ts
@@ -11,6 +11,13 @@ describe('MinLengthString Validation', () => {
     expect(error).toEqual(new InvalidParamError('any_field'))
   })
 
+  test('Should return a InvalidParamError if field is undefined', () => {
+    const sut = new MinLengthStringValidation('any_field', 5)
+    const error = sut.validate({})
+
+    expect(error).toEqual(new InvalidParamError('any_field'))
+  })
+
   test('Should not return if validation succeeds', () => {
     const sut = new MinLengthStringValidation('any_field', 5)
     const isValid = sut.validate({
