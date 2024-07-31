e87f7320041d6c5b03350fe5e57d5795ec50b698
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-07-31 15:53:32 -0300

    test: ensure JwtAdapter return a value on verify success

--- src/infra/crypto/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/crypto/jwt-adapter/jwt-adapter.spec.ts
@@ -47,5 +47,12 @@ describe('Jwt Adapter', () => {
       await sut.decrypt('any_token')
       expect(verifySpy).toHaveBeenCalledWith('any_token', 'secret')
     })
+
+    test('Should return a value on verify success', async () => {
+      const sut = new JwtAdapter('secret')
+      const value = await sut.decrypt('any_token')
+
+      expect(value).toBe('any_value')
+    })
   })
 })
