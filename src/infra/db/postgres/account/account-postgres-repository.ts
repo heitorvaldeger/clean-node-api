da133f2efd5e477dac6b7a0778ed1243a11321e2
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-07-31 16:45:34 -0300

    feat: ensure AccountPostgresRepository return an account on loadByToken success without role

--- src/infra/db/postgres/account/account-postgres-repository.ts
+++ src/infra/db/postgres/account/account-postgres-repository.ts
@@ -1,8 +1,13 @@
+import { ILoadAccountByTokenRepository } from '../../../../data/interfaces/db/account/load-account-by-token-repository'
 import { IAccountModel, IAddAccountModel, IAddAccountRepository } from '../../../../data/usecases/add-account/db-add-account-interfaces'
 import { ILoadAccountByEmailRepository, IUpdateAccessTokenRepository } from '../../../../data/usecases/authentication/db-authentication-interfaces'
 import { PostgresHelper } from '../helpers/postgres-helper'
 
-export class AccountPostgresRepository implements IAddAccountRepository, ILoadAccountByEmailRepository, IUpdateAccessTokenRepository {
+export class AccountPostgresRepository implements
+  IAddAccountRepository,
+  ILoadAccountByEmailRepository,
+  IUpdateAccessTokenRepository,
+  ILoadAccountByTokenRepository {
   async add (account: IAddAccountModel): Promise<IAccountModel> {
     const insertedRows = await PostgresHelper.getTable('accounts').insert<IAccountModel>(account).returning('*')
 
@@ -31,5 +36,18 @@ export class AccountPostgresRepository implements IAddAccountRepository, ILoadAc
     }
 
     return updatedRows[0]
+  }
+
+  async loadByToken (accessToken: string, role?: string): Promise<IAccountModel | null> {
+    let account: IAccountModel | null
+    if (!role) {
+      account = PostgresHelper.getTable('accounts').where('accessToken', accessToken)
+        .whereNull('role').first<IAccountModel>() as unknown as IAccountModel
+    } else {
+      account = PostgresHelper.getTable('accounts').where('accessToken', accessToken)
+        .where('role', role).first<IAccountModel>() as unknown as IAccountModel
+    }
+
+    return await new Promise(resolve => { resolve(account) })
   }
 }
