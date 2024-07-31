c83715bb890999d45916da393b15dbf66b32d42a
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-07-31 11:09:48 -0300

    refactor: move sut creation fo a factory method

--- src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -1,16 +1,30 @@
 import { IDecrypter } from '../../interfaces/crypto/decrypter'
 import { DbLoadAccountByToken } from './db-load-account-by-token'
 
+class DecrypterStub implements IDecrypter {
+  async decrypt (value: string): Promise<string> {
+    return await new Promise(resolve => { resolve('any_value') })
+  }
+}
+
+interface SutTypes {
+  sut: DbLoadAccountByToken
+  decrypterStub: DecrypterStub
+}
+const makeSut = (): SutTypes => {
+  const decrypterStub = new DecrypterStub()
+  const sut = new DbLoadAccountByToken(decrypterStub)
+
+  return {
+    decrypterStub,
+    sut
+  }
+}
+
 describe('DbLoadAccountByToken', () => {
   test('Should call Decrypter with correct value', async () => {
-    class DecrypterStub implements IDecrypter {
-      async decrypt (value: string): Promise<string> {
-        return await new Promise(resolve => { resolve('any_value') })
-      }
-    }
-    const decrypterStub = new DecrypterStub()
+    const { decrypterStub, sut } = makeSut()
     const decryptSpy = jest.spyOn(decrypterStub, 'decrypt')
-    const sut = new DbLoadAccountByToken(decrypterStub)
     await sut.load('any_token')
 
     expect(decryptSpy).toHaveBeenCalledWith('any_token')
