29d029dc06141fa66691240f659569d8478d89b4
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-07-31 16:46:20 -0300

    test: ensure AccountPostgresRepository return an account on loadByToken success without role

--- src/infra/db/postgres/account/account-postgres-repository.spec.ts
+++ src/infra/db/postgres/account/account-postgres-repository.spec.ts
@@ -111,4 +111,26 @@ describe('AccountPostgresRepository', () => {
       await expect(promise).rejects.toThrow(new Error('Update access token failure!'))
     })
   })
+
+  describe('loadByToken()', () => {
+    test('Should return an account on loadByToken success without role', async () => {
+      const accountTable = PostgresHelper.getTable('accounts')
+
+      await accountTable.insert({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        accessToken: 'any_token'
+      })
+
+      const sut = new AccountPostgresRepository()
+      const account = await sut.loadByToken('any_token')
+
+      expect(account).toBeTruthy()
+      expect(account?.id).toBeTruthy()
+      expect(account?.name).toBe('any_name')
+      expect(account?.email).toBe('any_email@mail.com')
+      expect(account?.password).toBe('any_password')
+    })
+  })
 })
