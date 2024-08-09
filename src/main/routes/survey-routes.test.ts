bee0bf1aa8057edab9134755f52925c35cd52815
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-08-09 00:21:31 -0300

    refactor: move accessToken creation to a factory method

--- src/main/routes/survey-routes.test.ts
+++ src/main/routes/survey-routes.test.ts
@@ -4,6 +4,30 @@ import app from '../app'
 import { PostgresHelper } from '../../infra/db/postgres/helpers/postgres-helper'
 import env from '../config/env'
 
+const makeAccessToken = async (): Promise<string> => {
+  const insertedRows = await PostgresHelper.getTable('accounts').insert({
+    name: 'any_name',
+    email: 'any_email@mail.com',
+    password: 'any_password',
+    role: 'admin'
+  }, '*')
+
+  if (!(insertedRows.length > 0)) {
+    throw new Error('Inserted rows failure!')
+  }
+
+  const accountId = insertedRows[0].id as string
+  const accessToken = sign(accountId, env.jwtSecret)
+  await PostgresHelper.getTable('accounts')
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    .where('id', insertedRows[0].id)
+    .update({
+      accessToken
+    }, '*')
+
+  return accessToken
+}
+
 describe('Surveys Routes', () => {
   beforeAll(() => {
     PostgresHelper.connect()
@@ -34,25 +58,7 @@ describe('Surveys Routes', () => {
     })
 
     test('Should return 201 on add survey success with accessToken', async () => {
-      const insertedRows = await PostgresHelper.getTable('accounts').insert({
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        password: 'any_password',
-        role: 'admin'
-      }, '*')
-
-      if (!(insertedRows.length > 0)) {
-        throw new Error('Inserted rows failure!')
-      }
-
-      const accountId = insertedRows[0].id as string
-      const accessToken = sign(accountId, env.jwtSecret)
-      await PostgresHelper.getTable('accounts')
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-        .where('id', insertedRows[0].id)
-        .update({
-          accessToken
-        }, '*')
+      const accessToken = await makeAccessToken()
 
       await request(app)
         .post('/api/surveys')
@@ -77,25 +83,7 @@ describe('Surveys Routes', () => {
     })
 
     test('Should return 204 on load surveys success with accessToken', async () => {
-      const insertedRows = await PostgresHelper.getTable('accounts').insert({
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        password: 'any_password',
-        role: 'admin'
-      }, '*')
-
-      if (!(insertedRows.length > 0)) {
-        throw new Error('Inserted rows failure!')
-      }
-
-      const accountId = insertedRows[0].id as string
-      const accessToken = sign(accountId, env.jwtSecret)
-      await PostgresHelper.getTable('accounts')
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-        .where('id', insertedRows[0].id)
-        .update({
-          accessToken
-        }, '*')
+      const accessToken = await makeAccessToken()
 
       await request(app)
         .get('/api/surveys')
@@ -105,25 +93,7 @@ describe('Surveys Routes', () => {
     })
 
     test('Should return 200 on load surveys success with accessToken', async () => {
-      const insertedRows = await PostgresHelper.getTable('accounts').insert({
-        name: 'any_name',
-        email: 'any_email@mail.com',
-        password: 'any_password',
-        role: 'admin'
-      }, '*')
-
-      if (!(insertedRows.length > 0)) {
-        throw new Error('Inserted rows failure!')
-      }
-
-      const accountId = insertedRows[0].id as string
-      const accessToken = sign(accountId, env.jwtSecret)
-      await PostgresHelper.getTable('accounts')
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-        .where('id', insertedRows[0].id)
-        .update({
-          accessToken
-        }, '*')
+      const accessToken = await makeAccessToken()
 
       await PostgresHelper.getTable('surveys').insert({
         question: 'any_question',
