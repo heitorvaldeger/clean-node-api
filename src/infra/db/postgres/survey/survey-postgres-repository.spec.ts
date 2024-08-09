2f75b80771ec3feace3b2cc3616523185f2fff32
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-08-09 00:01:01 -0300

    test: ensure SurveyPostgresRepository returns empty list if there are no surveys

--- src/infra/db/postgres/survey/survey-postgres-repository.spec.ts
+++ src/infra/db/postgres/survey/survey-postgres-repository.spec.ts
@@ -89,5 +89,11 @@ describe('SurveyPostgresRepository', () => {
       expect(surveys[0].question).toBe('any_question')
       expect(surveys[1].question).toBe('other_question')
     })
+
+    test('Should loadAll empty list', async () => {
+      const { sut } = makeSut()
+      const surveys = await sut.loadAll()
+      expect(surveys.length).toBe(0)
+    })
   })
 })
