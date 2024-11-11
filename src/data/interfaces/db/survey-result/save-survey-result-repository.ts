a02d25f557e82f2e5f1cfb8be787dbcd9a42b5b3
heitorvaldeger <aurelio.heitor@hotmail.com>
2024-11-10 23:20:25 -0300

    refactor: updates

--- src/data/interfaces/db/survey-result/save-survey-result-repository.ts
+++ src/data/interfaces/db/survey-result/save-survey-result-repository.ts
@@ -1,5 +1,5 @@
 import { SurveyResultModel } from '#domain/model/survey-result'
-import { SaveSurveyResultModel } from '#domain/usecases/interfaces/save-survey-result'
+import { SaveSurveyResultModel } from '#domain/usecases/interfaces/survey-result/save-survey-result'
 
 export interface ISaveSurveyResultRepository {
   save: (data: SaveSurveyResultModel) => Promise<SurveyResultModel | null>
